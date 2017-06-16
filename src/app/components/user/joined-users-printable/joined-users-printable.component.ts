7c138cb73303eafec04d4f5dfc9923590c180159
Sergey Eliseev <thefury014@gmail.com>
2017-06-16 13:44:24 +0900

    Print page

--- src/app/components/user/joined-users-printable/joined-users-printable.component.ts
+++ src/app/components/user/joined-users-printable/joined-users-printable.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'app/services/user.service';
+import { EventService, UserService } from 'app/services';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
-import { User, JoinedUser } from 'app/models/user';
+import { Event, User, JoinedUser } from 'app/models';
 
 @Component({
   selector: 'app-joined-users-printable',
@@ -13,11 +14,14 @@ import { User, JoinedUser } from 'app/models/user';
 export class JoinedUsersPrintableComponent implements OnInit {
 
   private eventId: number;
+  private event: Event;
+  private organizer: User;
   private users: JoinedUser[] = [];
   private id: number;
   private sub: Subscription;
 
   constructor(
+    private eventService: EventService,
     private userService: UserService,
     private route: ActivatedRoute,
     private router: Router
@@ -27,8 +31,14 @@ export class JoinedUsersPrintableComponent implements OnInit {
     this.sub = this.route.params.subscribe(params => {
       this.eventId = +params['id']; // (+) converts string 'id' to a number
     });
-    this.userService.getJoinedUsers(this.eventId)
-      .subscribe(data => { this.users = data.users; });
+
+    Observable.combineLatest(this.eventService.getEvent(this.eventId),
+      this.userService.getJoinedUsers(this.eventId))
+      .subscribe(data => {
+        this.event = data[0].event;
+        this.organizer = data[0].organizer;
+        this.users = data[1].users;
+      })
   }
 
   print() {
@@ -39,12 +49,55 @@ export class JoinedUsersPrintableComponent implements OnInit {
     popupWin.document.write(`
       <html>
         <head>
-          <title>Print tab</title>
+          <title>${this.event.title}. Список участников</title>
           <style>
-          //........Customized style.......
+          .body {
+            width: 210mm;
+            min-height: 297mm;
+            padding: 20mm;
+            margin: 10mm auto;
+            border: 1px #D3D3D3 solid;
+            border-radius: 5px;
+            background: white;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+          }
+
+          h1 {
+            font-family: 'Times New Roman', Times, serif;
+            font-weight: bold;
+            text-align: center;
+            font-size: 14pt;
+          }
+
+          .event-info {
+            font-family: 'Times New Roman', Times, serif;
+            font-size: 14pt;
+            margin-bottom: 21px;
+          }
+
+          table {
+            border: 1px solid #000;
+            border-collapse: collapse;
+            width: 100%;
+          }
+
+          table th, table td {
+            font-size: 14pt;
+            font-family: 'Times New Roman', Times, serif;
+            border: 1px solid #000;
+            padding: 2px;
+            text-align: center;
+          }
+
+          .subpage {
+            padding: 1cm;
+            border: 5px red solid;
+            height: 257mm;
+            outline: 2cm #FFEAEA solid;
+          }
           </style>
         </head>
-    <body onload="window.print();window.close()">${printContents}</body>
+        <body onload="window.print();window.close()">${printContents}</body>
       </html>`
     );
     popupWin.document.close();
